b0f11ccc236e100e5b48eeb4283c70cb60449bc1
fipar <fipar@8b890935-431d-0410-9bfb-ed3b0500c196>
2002-07-25 18:34:56 +0000

    modified slave_routine so we do the takeover first and then attempt the failover

--- mysql-ha/slave_routine.sh
+++ mysql-ha/slave_routine.sh
@@ -44,6 +44,7 @@ shouldrun || log "shouldrun was unsuccessfull (ok)"
 
 
 CHK_PROG="mysql.monitor --username=$MYSQL_USER --password=$MYSQL_PASSWORD --database=$MYSQL_DATABASE $MASTER_NODE"
+should_failover=0
 
 wrapper_safe_cmd.sh $MONITOR_PATIENCE $CHK_PROG && log "mysql responded (ok)" || {
 	sleep $MONITOR_CHK_THRESHOLD
@@ -57,25 +58,33 @@ wrapper_safe_cmd.sh $MONITOR_PATIENCE $CHK_PROG && log "mysql responded (ok)" ||
 				log "mysql.monitor was succesfull after restart (notify)"
 				exit 0
 			} 
-			log "mysql.monitor failed but $MASTER_NODE is running, going for the takeover (error)"
 			#this should change for a service running on the master node itself, so we can discover weird
 			#problems like a loop on the scsi driver, it has happened to me!. in this case, linux is running ok, 
 			#but it can't access the filesystem so nothing that depends on files can run (including mysql, but
 			#also including remote shells, or anything that uses files/sockets). 
-			wrapper_safe_cmd.sh $SSH_PATIENCE ssh root@$MASTER_NODE /usr/mysql-ha/failover.sh || {
-				log "could not failover.sh on $MASTER_NODE due to timeout abortion of safe_cmd.sh (error)"
-				}
-
+			should_failover=1
 		} || {
 			log "mysql.monitor failed but $MASTER_NODE was dead (error)"
 		}
-			/usr/mysql-ha/takeover.sh 
+			/usr/mysql-ha/takeover.sh
+			[ $should_failover -eq 1 ] && {
+				log "mysql.monitor failed but $MASTER_NODE is running, going for the takeover (error)"
+				wrapper_safe_cmd.sh $SSH_PATIENCE ssh root@$MASTER_NODE /usr/mysql-ha/failover.sh || {
+					log "could not failover.sh on $MASTER_NODE due to timeout abortion of safe_cmd.sh (error)"
+					}
+			}
+			should_exit=1
+			
 	}
 }
 
 }
 
-while :; do
+
+should_exit=0
+while [ $should_exit -eq 0 ]; do
 	main
 	sleep $SLAVE_SLEEP_TIME
 done
+
+log "exiting loop (after takeover) (notify)"
