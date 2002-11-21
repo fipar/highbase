beaa3bbbffa454f0a684959b1f7308338e0d22e8
fipar <fipar@8b890935-431d-0410-9bfb-ed3b0500c196>
2002-11-21 12:25:12 +0000

    using blocking io now, and a round robin server scheduler

--- mysql-hab/proxy/proxy.py
+++ mysql-hab/proxy/proxy.py
@@ -10,7 +10,7 @@ import os
 import sys
 import socket
 import string
-#import shm
+import syslog
 
 # ******************************************************
 # ### here's what you can modify to alter the behaviour
@@ -19,13 +19,14 @@ RECV_BUFF_SIZE = 1024
 PROXY_BYE_MSG = "mysql-proxy v0.1.0, admin server closing connection\n"
 PROXY_HELLO_MSG = "mysql-proxy v0.1.0, admin server starting\n"
 
+PIPE_PREFIX="/tmp/proxy_pipe"
 #mysql proxy server config
-LISTEN_ADDR = "127.0.0.1"
+LISTEN_ADDR = "0.0.0.0"
 LISTEN_BACKLOG = 512
 LISTEN_PORT = 3306
 
 #admin server config
-ADMIN_LISTEN_ADDR = "127.0.0.1"
+ADMIN_LISTEN_ADDR = "0.0.0.0"
 ADMIN_LISTEN_BACKLOG = 16
 ADMIN_LISTEN_PORT = 8888
 ADMIN_ALLOWED_ADDR = "127.0.0.1", "192.114.70.12"
@@ -41,25 +42,20 @@ s.listen(LISTEN_BACKLOG)
 servers = {}
 nextid = 0
 
-servers[0] = "127.0.0.1", 3308
+servers[0] = "192.114.70.12", 3308
+servers[1] = "192.114.70.22", 3308
+servers[2] = "192.114.70.23", 3306
 
 
-
-def process_client(sock, addr, nextid, servers):
-	if nextid == len(servers):
-		nextid = 1
-		currid = 0
-	else:
-		currid = nextid
-		nextid = nextid + 1
-	ip, port = servers[currid]
-	print "using server " + ip
+def process_client(sock, addr, ip, port):
 	cont = 1
 	asock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-	fam, stype, proto, cname, saddr = socket.getaddrinfo(ip,port)
-	asock.connect(saddr)
-	sock.setblocking(0)
-	asock.setblocking(0)
+	asock.connect((ip,port))
+	#sock.setblocking(0)
+	#asock.setblocking(0)
+	#try and except below wer placed for the use of
+	#non-blocking sockets. this was just a test and should be removed
+	#from the final version. 
 	while cont == 1:
 		if os.fork() != 0:
 			while cont == 1:
@@ -70,7 +66,7 @@ def process_client(sock, addr, nextid, servers):
 					asock.send(msg)
 				except:
 					cont = 1
-		else :
+		else:
 			while cont == 1:
 				try:
 					msg = asock.recv(RECV_BUFF_SIZE)
@@ -81,13 +77,20 @@ def process_client(sock, addr, nextid, servers):
 					cont = 1
 
 while 1:
-	#update_servers(servers)
 	con = s.accept()
 	sock, addr = con
+	peer, ignore =  addr
+	if nextid == (len(servers) - 1):
+		currid = nextid
+		nextid = 0
+	else:
+		currid = nextid
+		nextid = nextid + 1
 	if os.fork() != 0:
 		sock.close()
 	else:
-		print "processing client ", addr, " with server ", nextid
-		process_client(sock, addr, nextid, servers)
+		ip, port = servers[currid]
+		syslog("processing client "+peer":"+ignore+" with server "+currid+" ("+ip+":"+port+")")
+		process_client(sock, addr, ip, port)
 		sock.close()
 		sys.exit(0)
