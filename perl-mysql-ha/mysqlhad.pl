d390f7608ec7f34b317a9f2ea04a8b1f62331317
dopheide <dopheide@8b890935-431d-0410-9bfb-ed3b0500c196>
2003-05-19 21:16:45 +0000

    changes to mysqlhad.pl to fix spelling error and add GetMasterHost()

--- perl-mysql-ha/mysqlhad.pl
+++ perl-mysql-ha/mysqlhad.pl
@@ -23,7 +23,12 @@ $testdb = $conf{'MYSQL_DATABASE'};
 $localuser = $conf{'DB_USER'};
 $localpass = $conf{'DB_PASSWORD'};
 
+## Determine masterhost
 ## can determine masterhost from my.cnf or from show slave status
+## or from mysql startup script (/etc/init.d/mysql or mysqld)
+$conf{'MASTER_HOST'} = &GetMasterHost;
+print "master host is $conf{'MASTER_HOST'}\n";
+
 
 ## can match against a list of slaves from the config file
 if($conf{'SLAVE'} =~ /$hostname/i){
@@ -85,4 +90,63 @@ sub GetSlaveThreadStatus(){
     $dbh->disconnect();
 
     return $status;
+}
+
+
+sub GetMasterHost(){
+    local($master) = "";	  
+    if(-e "/etc/my.cnf"){
+	if(!(open(MYCNF,"/etc/my.cnf"))){
+	    print "/etc/my.cnf exists but can't be opened\n";
+	    #non-fatal error
+	}else{
+	    while(<MYCNF>){
+		if( /^\s*master-host\s*=\s*[\'\"]*([\w\-\.]+)/i ){
+		    $master = $1;
+		    last;
+		}
+	    }
+	    close(MYCNF);
+	}
+	if($master eq ""){
+	    ## look in startup script(s)
+	    if((-e "/etc/init.d/mysql") || (-e "/etc/init.d/mysqld")){
+		if((!(open(MYSQL,"/etc/init.d/mysql"))) &&
+		   (!(open(MYSQL,"/etc/init.d/mysqld")))){
+		    print "Failed to open MySQL startup script\n";
+		    #non-fatal error
+		}else{
+		    while(<MYSQL>){
+			if( /master-host=[\'\"]*([\w\-\.]+)/i ){
+			    $master = $1;
+			    last;
+			}
+		    }
+		    close(MYSQL);
+		}
+	    }
+	}
+	if($master eq ""){
+	    ## try to determine master-host from SHOW SLAVE STATUS
+	    my $dbh = DBI->connect( "DBI:$mode:$testdb:localhost:$port", $localuser, $localpass );
+	    if(!($dbh)){
+		print "failed to connect to local mysql daemon, do something\n";
+	    }
+	    
+	    $query = "SHOW SLAVE STATUS";
+	    my $sth = $dbh->prepare($query);
+	    $sth->execute();
+	    $ref = $sth->fetchrow_arrayref();
+	    $master = $$ref[0];
+	    $sth->finish();
+	    $dbh->disconnect();
+	}
+    }
+    
+    if($master eq ""){
+	print "Could not determine MASTER_HOST\n";
+	exit 0;
+    }
+
+    return $master;
 }
