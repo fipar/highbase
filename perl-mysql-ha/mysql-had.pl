0d1d54d9a33482a7c3e3403307e2a5c0cd884aaf
dopheide <dopheide@8b890935-431d-0410-9bfb-ed3b0500c196>
2003-05-28 16:28:35 +0000

    Added Mail::Send functionality to Log() and renamed mysqlhad.pl to mysql-had.pl

--- perl-mysql-ha/mysql-had.pl
+++ perl-mysql-ha/mysql-had.pl
@@ -9,6 +9,7 @@ require 'readconf.pl';
 use DBI;
 use Net::Ping;
 use Sys::Syslog;
+use Mail::Send;
 
 # read configuration info
 %conf = &ReadConf;
@@ -171,4 +172,24 @@ sub Log($level,$message){
     openlog("mysql-had", 'pid', 'user');
     syslog($level,$message);
     closelog();
+
+    if($conf{'NOTIFY_METHODS'} =~ /email/i){
+	if($conf{'NOTIFY_EMAIL'} =~ /([\w\-]+\@[\w\-\.]+)/){
+	    $address = $1;
+	    print "send email to $address\n";
+	    $msg = new Mail::Send;
+	    $msg->to($address);
+	    $msg->subject("$hostname mysql-ha");
+	    $fh = $msg->open('sendmail');
+	    print $fh "$hostname mysql-ha\n\n$message";
+	    $fh->close;
+	}else{
+	    ## don't call Log(), would result in a loop
+	    $message = "Failed to send email, NOTIFY_EMAIL invalid";
+	    $level = "info";
+	    openlog("mysql-had", 'pid', 'user');
+	    syslog($level,$message);
+	    closelog();
+	}
+    }
 }
