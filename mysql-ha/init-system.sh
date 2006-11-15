b27eff5f0ed749aa4382a808a4b804724eecac8e
fipar <fipar@8b890935-431d-0410-9bfb-ed3b0500c196>
2006-11-15 15:06:29 +0000

    Added a script to reinitialize a test system for mysql-ha

---
+++ mysql-ha/init-system.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+# init-system.sh
+# prepares a test system for mysql-ha after an installation has already been done
+# 
+# ******************************************
+# THIS SCRIPT IS USEFUL ONLY ON TEST SYSTEMS
+# ******************************************
+
+rm -rf /usr/mysql-ha
+userdel mysqlha
+groupdel mysqlha
+grep -v mysql-ha /etc/bashrc > tmpf && mv -f tmpf /etc/bashrc
+grep -v mysqlha /etc/sudoers > tmpf && mv -f tmpf /etc/sudoers
+
